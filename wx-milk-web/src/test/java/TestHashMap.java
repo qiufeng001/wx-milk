403de6f85042d28d0c371c7df4652c9809523a65
qiufeng001 <2227052723@qq.com>
2018-12-12 16:19:35 +0800

    测试

--- wx-milk-web/src/test/java/TestHashMap.java
+++ wx-milk-web/src/test/java/TestHashMap.java
@@ -4,6 +4,9 @@ public class TestHashMap {
     public static void main(String args[]) {
         HashMap<String, String> map = new HashMap<>();
 
+
         map.put("test", "test");
+        map.put("test1", "test2");
+
     }
 }
